abec0356e78f99aa98d33eeab786ceb7468940b3
Rabbit13245 <dimkazayats@gmail.com>
2021-03-23 11:03:52 +0300

    add project

---
+++ YandexStocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexStocks
+//
+//  Created by Admin on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
